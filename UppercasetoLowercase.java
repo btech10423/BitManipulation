3c1a93408a61ecfa987dce372eedc76c6d81a734
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-09 11:03:09 +0530

    Add files via upload

---
+++ UppercasetoLowercase.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class UppercasetoLowercase {
+    public static void main(String args[]){
+        for(char i='A';i<='Z';i++){
+System.out.print((char)(i|' '));
+        }
+    }
+}
